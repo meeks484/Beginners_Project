71ac2b5ff1f1069e9b6e7a7a4ecd1c5285378321
meeks484 <meeks484@gmail.com>
2022-06-14 22:31:50 -0600

    Commit_New_Methode

--- src/TestClass1.java
+++ src/TestClass1.java
@@ -1,6 +1,11 @@
 public class TestClass1 {
 
+    public static void New_Method(){
+        System.out.println("Test This Method");
+    }
+
     public static void main(String[] Args){
         System.out.println("Test This Class");
+        New_Method();
     }
 }
