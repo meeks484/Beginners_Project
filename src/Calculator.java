8908e2588a45bdba291efb220131d91d510b10dd
meeks484 <meeks484@gmail.com>
2022-06-19 22:14:00 -0600

    Implimented suggested changes by Cu. Added notes, corrected naming style

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,53 +1,36 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        float[] numArray = {2,5,8,5,10};
-        String[] operArray = {"+","*","/","-"};
-        float results = Calculate(numArray, operArray);
+        float[] numberArray = {2,5,8,5,2,15,-7};
+        String[] operandArray = {"Add","Mul","Div","Sub","Sub","Mul"};
+        float results = calculate(numberArray, operandArray);
         System.out.println(results);
     }
-    public static float Calculate(float[] numbers,String[] operands){
-        float[] result = new float[operands.length];
-        float[] multResult = new float[operands.length];
-        String[] multOper = new String[operands.length-1];
-        float[] divResult = new float[operands.length-1];
-        String[] divOper = new String[multOper.length-1];
-        float[] addResult=new float[multOper.length-1];
-        String[] addOper = new String[divOper.length-1];
-        int instances=0;
+    public static float calculate(float[] numbers,String[] operands){
 
-            for (int i = 0; i < operands.length; i++) {
-                if (operands[i].equals("*")) {
-                    instances++;
-                    multResult[i] = numbers[i] * numbers[i + 1];
-                    if (i!=operands.length-1){multOper[i] = operands[i + 1];}
-                } else {
-                    multResult[i] = numbers[i+instances];
-                    if (i!=operands.length-1){multOper[i]=operands[i+instances];}
-                }
+        float productQuotientGroup = numbers[0]; //Represents the product or quotient of a group of numbers
+        float result = 0;
+
+        //Loop through each number, add, subtract, multiply or divide based on the operand
+        for (int i = 0; i< numbers.length; i++) {
+
+            // Check if end of array
+            if (i == operands.length){break;}
+
+            //If the operand is "Add" or "Sub", then add the productQuotientGroup into "result", and then
+            //  re-initialize the productQuotientGroup variable based on the operand
+            if (operands[i].equals("Add") || operands[i].equals("Sub")) {
+                result += productQuotientGroup;
+                productQuotientGroup=operands[i].equals("Add") ? numbers[i+1] : -numbers[i+1];
             }
-            instances=0;
-            for (int i = 0; i < multOper.length;i++){
-                if (multOper[i].equals("/")){
-                    instances++;
-                    divResult[i]=multResult[i]/multResult[i+1];
-                    if (i!=multOper.length-1){divOper[i]=multOper[i+instances];}
-                } else {
-                    divResult[i]=multResult[i+instances];
-                    if (i!=multOper.length-1){divOper[i]=multOper[i+instances];}
-                }
-            }
-            instances=0;
-            for (int i=0; i<divOper.length;i++){
-                if (divOper[i].equals("+")){
-                    instances++;
-                    addResult[i]=divResult[i]+divResult[i+1];
-                    if (i!=divOper.length-1){addOper[i]=divOper[i+instances];}
-                } else{
-                    addResult[i]=divResult[i+instances];
-                    if (i!=divOper.length-1){addOper[i]=divOper[i+instances];}
-                }
-            }
-        return(addResult[1]-addResult[0]);
+
+            //Calculate the product or quotient, depending on the operand
+            if (operands[i].equals("Mul")){productQuotientGroup *= numbers[i + 1];}
+            if (operands[i].equals("Div")){productQuotientGroup = productQuotientGroup / numbers[i + 1];            }
+        }
+
+        //add the final "productQuotientGroup" into the result, and return the results.
+        result += productQuotientGroup;
+        return(result);
     }
 }
